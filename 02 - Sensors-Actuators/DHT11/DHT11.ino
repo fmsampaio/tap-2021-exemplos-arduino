902e817de0f26c6ebd84b2cea5beb10c7bb63ad5
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2020-02-26 14:26:07 -0300

    Updated examples and schemas of Sensors-actuators

--- 02 - Sensors-Actuators/DHT11/DHT11.ino
+++ 02 - Sensors-Actuators/DHT11/DHT11.ino
@@ -1,22 +1,13 @@
 #include "DHT.h"
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
-
 #define DHTTYPE DHT11   // DHT 11
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
-
   dht.begin();
 }
 
@@ -24,8 +15,6 @@ void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
