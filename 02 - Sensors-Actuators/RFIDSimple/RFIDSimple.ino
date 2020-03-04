e8acb5946cf944cfdea21b7e12ddaa2facf288a2
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2020-03-04 08:29:30 -0300

    Added I2C display schema and RFID examples

---
+++ 02 - Sensors-Actuators/RFIDSimple/RFIDSimple.ino
@@ -0,0 +1,11 @@
+void setup() {
+  //TODO (1) inicializar a Serial (9600 bps)
+  //TODO (2) montar o protótipo de acordo com o esquemático
+  //TODO (3) inicializar o RFID (chamar função)
+}
+
+void loop() {
+  //TODO (4) fazer verificação da presença de uma tag RFID
+  //TODO (5) acessar o ID da tag capturada pelo sensor
+  //TODO (6) caso afirmativo, imprimir via porta serial o ID da tag
+}
